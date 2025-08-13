49abd80e4deb3e3a5ae985939dbe7212dd22d7a4
MakszCsepes <guitarist.tolkienist@gmail.com>
2025-08-13 15:16:09 +0100

    Moved Class logic into dedicated file | Added initial tests coverage

--- main.py
+++ main.py
@@ -1,156 +1,35 @@
 #! /usr/bin/env python3
 
-import argparse
-import json
-import sys
+import sys, json
 from typing import List, Dict
+from task_tracker_class import TaskTracker
 
-class TaskTracker:
-    ACTION_KEY_LIST = "list"
-    ACTION_KEY_LIST_DONE = "done"
-    ACTION_KEY_LIST_IN_PROGRESS = "in-progress"
-    ACTION_KEY_LIST_TODO = "todo"
+def configCreate(config_filename: str) -> Dict:
+    import os
+    if not os.path.isfile(config_filename):
+        raise FileNotFoundError(f"Configuration file '{config_filename}' doesn't exist!")
 
-    ACTION_KEY_ADD = "add"
-    ACTION_KEY_DELETE = "delete"
-
-    def __init__(self, config: Dict):
-        self.tasks = []
-        self.config = config
-        self.actions = []
-
-        self.configureParser()
-    
-    def set_action(self, arg_index, total_args):
-        arg = sys.argv[arg_index]
-        list_actions = self.config["actions_template"]["list"]
-
-        # LIST
-        if arg == self.ACTION_KEY_LIST:
-            if arg_index + 1 >= total_args or sys.argv[arg_index + 1] not in list_actions:
-                raise ValueError("List action is not specified correctly")
-            else:
-                self.actions.append([arg, sys.argv[arg_index + 1]])
-                return arg_index + 1
-        # ADD
-        elif arg == self.ACTION_KEY_ADD:
-            if arg_index + 1 >= total_args or not isinstance(sys.argv[arg_index + 1], str):
-                raise ValueError("Add action is not specified correctly")
-            else:
-                self.actions.append([arg, sys.argv[arg_index + 1]])
-                return arg_index + 1
-
-        # UPDATE
-        elif arg == "update":
-            pass
-
-        # DELETE
-        elif arg == "delete":
-            pass
-
-        # MARK-IN-PROGRESS
-        elif arg == "mark-in-progress":
-            pass
-
-        # MARK-DONE
-        elif arg == "mark-done":
-            pass
-
-        return arg_index
-
-    def parseArguments(self):
-        arguments_n = len(sys.argv)
-        if arguments_n < 2:
-            return
-        
-        for arg_i in range(1, arguments_n):
-            arg_i = self.set_action(arg_i, arguments_n)
-
-
-    def configureParser(self):
-        if not self.config:
-            raise ValueError("Config is not configured")
-
-        try:
-            self.parseArguments()
-        except ValueError as e:
-            print(e)
-
-
-    def run_action(self, action):
-        named_action = action[0]
-
-        if named_action == TaskTracker.ACTION_KEY_LIST:
-            self.list_tasks(action[1])
-        elif named_action == TaskTracker.ACTION_KEY_ADD:
-            self.add_task(action[1])
-
-
-    def run(self):
-        
-        
-        for a in self.actions:
-            self.run_action(a)
-        
-        
-
-    def list_tasks(self, status: str):
-        # EGDE CASES what if the file very long, we'd want to read in batches
-        print(f"Here's a list of your {status} tasks:")
-        tasks = self.read_the_tasks(status=status)
-
-        for t in tasks:
-            print(f"{t['id']} - {t['description']} - {t['status']}")
-
-
-    def add_task(self, task: str):
-        # EDGE CASES what if the task exists?
-        tasks = self.read_the_tasks()
-        
-        new_task = {
-            "id": "100", # TODO generate ID
-            "description": task,
-            "status": self.ACTION_KEY_LIST_TODO,
-            "createdAt": "now", # TODO generate the date
-            "updatedAt": "now"  # TODO generate the date
-        }
-
-        tasks.append(new_task)
-
-        self.write_the_tasks(tasks)
-    
-    def write_the_tasks(self, tasks):
-        # EDGE CASES: verify the input
-
-
-        with open(self.config["file_to_write"], "w") as f:
-            json.dump({"tasks": tasks}, f, indent=3)
-
-
-    def read_the_tasks(self, status: str="") -> List:
-        tasks = []
-        
-        # read the tasks from the json file
-        with open(self.config["file_to_write"], "r") as f:
-            if status:
-                tasks = [t for t in json.load(f)["tasks"] if t["status"] == status]
-            else:
-                tasks = [t for t in json.load(f)["tasks"]]
-        return tasks
-
-def main():
     config = {}
-    with open("config.json", "r") as f:
+    with open(config_filename, "r") as f:
         config = json.load(f)
 
-    tracker = TaskTracker(config)
-    
-    if not tracker:
+    return config
+
+
+
+def main():
+    config: Dict = {}
+    try:
+        config = configCreate("config.json")
+    except FileExistsError as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+    tracker = TaskTracker(config)    
+    if not tracker.is_valid():
         raise ValueError("Tracker is not configured")
 
     tracker.run()
-
-
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -1,2 +1,113 @@
-def test_1():
-    pass
+from main import configCreate
+from typing import Dict, List
+from task_tracker_class import TaskTracker
+import pytest
+
+# ==== CONFIGURATION ====
+
+def test_create_config_raises_exception_if_configfile_invalid():
+    with pytest.raises(FileNotFoundError) as excinfo:
+        configCreate("invalid_file")
+
+    assert "Configuration file 'invalid_file' doesn't exist!" in str(excinfo.value)
+
+def test_create_config_valid():
+    config = configCreate("config.json")
+    assert isinstance(config, Dict)
+
+
+# ==== TASK TRACKER CLASS ====
+
+# === READ TASKS ===
+
+@pytest.fixture
+def tracker() -> TaskTracker:
+    return TaskTracker(configCreate("config.json"))
+
+@pytest.fixture
+def tasks_dataset() -> object:
+    return {"tasks": [
+        {
+            "id": "1",
+            "description": "Description 1",
+            "status": "in-progress",
+            "createdAt": "now",
+            "updatedAt": "now"
+        },
+        {
+            "id": "2",
+            "description": "Description 2",
+            "status": "todo",
+            "createdAt": "now",
+            "updatedAt": "now"
+        },
+        {
+            "id": "2",
+            "description": "Description 2",
+            "status": "done",
+            "createdAt": "now",
+            "updatedAt": "now"
+        },
+    ]}
+
+def test_can_read_all_tasks(tracker):
+    tasks = tracker.read_the_tasks()
+    assert isinstance(tasks, List)
+    assert len(tasks) != 0
+
+def test_can_read_tasks_from_dataset(tracker, tasks_dataset):
+    tasks = tracker.read_the_tasks(status = "", read_from_dataset = True, dataset = tasks_dataset)
+
+    assert isinstance(tasks, List)
+    assert len(tasks) == 3
+
+def test_read_the_tasks_raises_exception_if_dataset_not_provided(tracker):
+    with pytest.raises(ValueError) as excinfo:
+        tracker.read_the_tasks("", read_from_dataset = True)
+
+    assert "The dataset to read from has not been provided!" in str(excinfo.value)
+
+@pytest.mark.parametrize("task_status, expected_count", [
+    ("todo", 1),
+    ("in-progress", 1),
+    ("done", 1),
+])
+def test_can_read_tasks_by_status(tracker, task_status, expected_count, tasks_dataset):
+    tasks = tracker.read_the_tasks(task_status, read_from_dataset = True, dataset = tasks_dataset)
+    
+    assert all([task["status"] == task_status for task in tasks])
+    assert len(tasks) == expected_count
+
+
+# === WRITE TASKS ===
+
+def test_can_serialize_generated_task(tracker):
+    task = tracker.generate_new_task("new_task")
+    print(f"Task: {task}")
+
+    import json
+    assert json.dumps(task)
+    
+def test_can_write_tasks(tracker):
+    file_to_write = "test_file.json"
+    tasks = [tracker.generate_new_task("new_task")]
+    tracker.write_the_tasks(tasks, file_to_write)
+
+    import os, json
+    assert os.path.isfile(file_to_write)
+
+    with open(file_to_write, "r") as f:
+        contents = json.load(f)
+        assert "tasks" in contents
+        assert isinstance(contents["tasks"], List)
+        assert len(contents["tasks"]) == 1
+        assert all(field in contents["tasks"][0] for field in ('id', 'description', 'status', 'createdAt', 'updatedAt'))
+    
+    os.remove(file_to_write)
+    assert not os.path.isfile(file_to_write)
+
+# === SET ACTION ===
+
+
+
+# ==== CLI-Level TESTS ====
